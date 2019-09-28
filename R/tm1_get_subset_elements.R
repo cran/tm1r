475fef1348f8bc9875cf4311a310d96b95625256
Muhammed Ali Onder <muhammedalionder@gmail.com>
2019-09-28 04:00:03 +0000

    version 1.1.3

---
+++ R/tm1_get_subset_elements.R
@@ -0,0 +1,51 @@
+tm1_get_subset_elements <- function(tm1_connection, dimension, subset) {
+
+  tm1_adminhost <- tm1_connection$adminhost
+  tm1_httpport <- tm1_connection$port
+  tm1_auth_key <- tm1_connection$key
+  tm1_ssl <- tm1_connection$ssl
+
+  # added because some http does not know space
+  dimension <- gsub(" ", "%20", dimension, fixed=TRUE)
+  subset_new <- gsub(" ", "%20", subset, fixed=TRUE)
+
+  u1 <- ifelse(tm1_ssl==TRUE, "https://", "http://")
+  #u1 <- "https://"
+  u2 <- tm1_adminhost
+  u3 <- ":"
+  u4 <- tm1_httpport
+  u5 <- "/api/v1/Dimensions('"
+  u6 <- dimension
+  u7 <- "')/Hierarchies('"
+  u8 <- dimension
+  u9 <- "')/Subsets('"
+  u10 <- subset_new
+  u11 <- "')/Elements?$select=Name"
+
+  # url development
+  url <- paste0(u1, u2, u3, u4, u5, u6, u7, u8, u9, u10, u11)
+  #url = "Dimensions('DMeasure')/Hierarchies('DMeasure')/Subsets('Accounts to loop')/Elements?$select=Name"
+
+  # post request
+  tm1_process_return <-
+    httr::GET(url,
+              httr::add_headers("Authorization" = tm1_auth_key))
+
+  # check return if error
+  if (is.null(jsonlite::fromJSON(httr::content(tm1_process_return, "text"))$error$message) == FALSE) {
+    message(jsonlite::fromJSON(httr::content(tm1_process_return, "text"))$error$message)
+    stop()
+  }
+
+  # make it proper
+  tm1_sub_els <- jsonlite::fromJSON(httr::content(tm1_process_return, "text"))$value$Name
+
+  #change to data frame
+  #tm1_sub_els <- as.data.frame(tm1_sub_els)
+  #colnames(tm1_sub_els)[1] <- subset
+  #return
+  return(tm1_sub_els)
+
+
+
+}
